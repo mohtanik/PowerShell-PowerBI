8621ccd649392b5df625994c736407f122f758c3
Nikunj Mohta <84266642+mohtanik@users.noreply.github.com>
2025-06-04 16:18:36 +0530

    Add files via upload

---
+++ Export Reports (PBIX, RDL) in Workspace.ps1
@@ -0,0 +1,15 @@
+﻿Connect-PowerBIServiceAccount
+
+$groupId = "YOUR WORKSPACE ID"
+$restURL = "https://api.powerbi.com/v1.0/myorg/groups/$groupId/reports"
+$restResponse = Invoke-PowerBIRestMethod -Url $restURL -Method GET | ConvertFrom-Json
+
+$OutFolder = "C:\New Folder\" 
+
+foreach ($item in $restResponse.value) {
+    $OutFile = $OutFolder + $item.name + ($(if ($item.reportType -eq "PowerBIReport") {".pbix"} elseif ($item.reportType -eq "PaginatedReport") {".rdl"} else {""}))
+
+    if ($OutFile -ne $OutFolder + $item.name) {
+        Invoke-PowerBIRestMethod -Method GET -Url "https://api.powerbi.com/v1.0/myorg/groups/$groupId/reports/$($item.id)/Export" -OutFile $OutFile
+    }
+}
